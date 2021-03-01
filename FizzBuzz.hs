c9dc2044e00ef94f108b271ebb048fc1f803c512
AidanP771 <aidanm771@outlook.com>
2021-03-01 18:06:48 -0500

    Add files via upload

---
+++ FizzBuzz.hs
@@ -0,0 +1,116 @@
+-- FizzBuzz rules:
+-- print numbers from 1 to 100
+-- if number divisible by 3, print Fizz
+-- if number divisible by 5, print Buzz
+-- if number divisible by both 3 and 5, print FizzBuzz
+-- otherwise print the number
+
+-- example output: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz
+
+
+module Main where
+
+main :: I0 ()
+main = putStrLn "SOCKS = Knocked off"
+
+main = putStrLn "\
+\1\n\
+\2\n\
+\Fizz\n\
+\4\n\
+\Buzz\n\
+\Fizz\n\
+\7\n\
+\8\n\
+\Fizz\n\
+\Buzz\n\
+\11\n\
+\Fizz\n\
+\13\n\
+\14\n\
+\FizzBuzz\n\
+\16\n\
+\17\n\
+\Fizz\n\
+\19\n\
+\Buzz\n\
+\Fizz\n\
+\22\n\
+\23\n\
+\Fizz\n\
+\Buzz\n\
+\26\n\
+\Fizz\n\
+\28\n\
+\29\n\
+\FizzBuzz\n\
+\31\n\
+\32\n\
+\Fizz\n\
+\34\n\
+\Buzz\n\
+\Fizz\n\
+\37\n\
+\38\n\
+\Fizz\n\
+\Buzz\n\
+\41\n\
+\Fizz\n\
+\43\n\
+\44\n\
+\FizzBuzz\n\
+\46\n\
+\47\n\
+\Fizz\n\
+\49\n\
+\Buzz\n\
+\Fizz\n\
+\52\n\
+\53\n\
+\Fizz\n\
+\Buzz\n\
+\56\n\
+\Fizz\n\
+\58\n\
+\59\n\
+\FizzBuzz\n\
+\61\n\
+\62\n\
+\Fizz\n\
+\64\n\
+\Buzz\n\
+\Fizz\n\
+\67\n\
+\68\n\
+\Fizz\n\
+\Buzz\n\
+\71\n\
+\Fizz\n\
+\73\n\
+\74\n\
+\FizzBuzz\n\
+\76\n\
+\77\n\
+\Fizz\n\
+\79\n\
+\Buzz\n\
+\Fizz\n\
+\82\n\
+\83\n\
+\Fizz\n\
+\Buzz\n\
+\86\n\
+\Fizz\n\
+\88\n\
+\89\n\
+\FizzBuzz\n\
+\91\n\
+\92\n\
+\Fizz\n\
+\94\n\
+\Buzz\n\
+\Fizz\n\
+\97\n\
+\98\n\
+\Fizz\n\
+\Buzz"\
